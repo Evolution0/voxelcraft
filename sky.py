6c25ba358b328a215644f712de7458bbccd87161
Calinescu Mihai <69072635+CMihai99@users.noreply.github.com>
2021-04-09 22:50:34 +0300

    Update sky.py

--- sky.py
+++ sky.py
@@ -2,16 +2,16 @@ from ursina import *
 
 
 # 7. Assets
-sky_texture = load_texture('assets/skybox.png')
+#sky_texture = load_texture('assets/skybox.png')
 
 
 # 9. Create sky
 class Sky(Entity):
     def __init__(self):
         super().__init__(
-            parent = scene,          # Specifies parent of sky so it scales properly
-            model = 'sphere',        # Specifies sky model
-            texture = sky_texture,   # Sky texture
-            scale = 1000,            # Increases size drastically
-            double_sided = True      # See the sphere when you are in it
+            parent = scene,                 # Specifies parent of sky so it scales properly
+            model = 'sphere',               # Specifies sky model
+            texture = 'assets/skybox.png',  # Sky texture
+            scale = 1000,                   # Increases size drastically
+            double_sided = True             # See the sphere when you are in it
         )
